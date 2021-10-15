92cce2491b882af84228bec8c01d8ad1f7090726
Leonardo Pintor <jpintormore@uniminuto.edu.co>
2021-10-15 09:05:36 -0500

    se enlaza el html de product cart en app y se linkea hoja de estilos en productcart

--- artisanMarket/src/app.js
+++ artisanMarket/src/app.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 3000;
 
 const publicPath = path.resolve(__dirname, './public');
 const homePath = path.resolve(__dirname, './views/index.html');
+const CartPath = path.resolve(__dirname, './views/productCart.html');
 
 app.use(express.static(publicPath));
 
 app.get('/', function(req, resp) {
     resp.sendFile(homePath);
+})
+app.get('/', function(req, resp) {
+    resp.sendFile(CartPath);
 })
 
 app.listen(port, () => console.log(`Server is running in port ${port}`));
