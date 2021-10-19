078bc424b1ff34d6d466e1807a5e0e33719dcf01
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2021-10-18 19:50:43 -0500

    avances en el login page

--- artisanMarket/app.js
+++ artisanMarket/app.js
@@ -3,9 +3,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const publicPath = path.resolve(__dirname, './public');
-const homePath = path.resolve(__dirname, './views/index.html');
-const CartPath = path.resolve(__dirname, './views/productCart.html');
+const publicPath = path.resolve(__dirname, './src/public');
+const homePath = path.resolve(__dirname, './src/views/index.html');
+const CartPath = path.resolve(__dirname, './src/views/productCart.html');
+const loginPath = path.resolve(__dirname, './src/views/login.html')
 
 app.use(express.static(publicPath));
 
@@ -14,6 +15,9 @@ app.get('/', function(req, res) {
 })
 app.get('/productCart', function(req, res) {
     res.sendFile(CartPath);
+})
+app.get('/login', (req, res) => {
+    res.sendFile(loginPath);
 })
 
 app.listen(port, () => console.log(`Server is running in port ${port}`));
