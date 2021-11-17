3e329a72d182622bf9149b06b6d4b76e79728528
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2021-11-17 13:20:29 -0500

    Add: ase agrega el CRUD correspondiente al archivo users.js

---
+++ artisanMarket/src/data/modelUsers.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+const DB = require('./users.json');
+
+const modelUsers = {
+    getUsers: function () {
+        return JSON.parse(fs.readFileSync(path.resolve(__dirname, './users.json'), { encoding: "utf-8" }));
+    },
+    isExist: function (id) {
+        const isExist = DB.find(user => user.id == id)
+        if (isExist) {
+            return true
+        } else {
+            return false
+        }
+    },
+    createUser: function (newUser) {
+        const users = DB
+        if (this.isExist(newUser.id)) {
+            return console.log("El usuario con id " + newUser.id + " ya existe.");
+        } else {
+            users.push(newUser);
+            fs.writeFileSync(
+                path.resolve(__dirname, './users.json'),
+                JSON.stringify(users, null, 4),
+                { encoding: "utf-8" }
+            );
+            return console.log("Agregado con éxito.");
+        }
+    },
+    updateUser: function (userId, userUpdated) {
+        const indiceBuscado = this.getUsers().findIndex(user => user.id == userId);
+        if (indiceBuscado < 0) {
+            return console.log("El usuario con id " + userId + " no esta registado en la base de datos");
+        } else {
+            let newDB = this.getUsers();
+            newDB[indiceBuscado] = userUpdated
+            fs.writeFileSync(
+                path.resolve(__dirname, './users.json'),
+                JSON.stringify(newDB, null, 4),
+                { encoding: "utf-8" }
+            );
+            return console.log("Actualizado con éxito");
+        }
+    },
+    deleteUser: function (userId) {
+        const newDB = this.getUsers().filter(user => user.id != userId);
+        this.getUsers().push(newDB);
+        fs.writeFileSync(
+            path.resolve(__dirname, './users.json'),
+            JSON.stringify(newDB, null, 4),
+            { encoding: "utf-8" }
+        );
+    }
+}
+
+
+module.exports = modelUsers;
