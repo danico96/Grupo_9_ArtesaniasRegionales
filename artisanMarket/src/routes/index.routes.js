79ba66345aefc4ac54ee1d25f3c79addd0fcc8ae
Leonardo Pintor <jpintormore@uniminuto.edu.co>
2021-11-19 14:32:36 -0500

    se realiza traslado de archivos modelo a carpeta model y se agrega lineas de multer en routes

--- artisanMarket/src/routes/index.routes.js
+++ artisanMarket/src/routes/index.routes.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controller/index.controller');
 const productController = require('../controller/productController');
+const multer = require("multer");
+const path = require("path");
 
-
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "../public/img"));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix =
+        Date.now() + "-" + Math.round(Math.random() * 1e9) + file.originalname;
+      cb(null, file.fieldname + "-" + uniqueSuffix);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
 // Acá definimos las rutas
 router.get('/', controller.home); /* GET home page. */
 
@@ -14,6 +27,9 @@ router.get('/productCart', controller.productCart); /* GET product cart page. */
 router.get('/productDetail', productController.detail); /* GET product detail page. */
 
 router.get('/register', controller.register); /* GET register page. */
+
+router.get('/productCreate', productController.create); /* GET product create page. */
+router.post('/productCreate', );
 
 router.get('/productEdit', productController.edit); /* GET product edit page. */
 
