9b2394045bb693a0e12684191c6f20794fa45eef
Leonardo Pintor <jpintormore@uniminuto.edu.co>
2021-11-25 15:13:28 -0500

    se corrige ruta de acceso a syles

--- artisanMarket/src/app.js
+++ artisanMarket/src/app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 app.use('/', rutas);
 
